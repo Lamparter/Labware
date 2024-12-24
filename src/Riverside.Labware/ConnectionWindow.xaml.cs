efdcf024c93f27763d6996e9d9d627403a92e6c1
Lamparter <71598437+Lamparter@users.noreply.github.com>
2024-12-24 18:59:03 +0000

    Add `ConnectionWindow`
    W.I.P.

---
+++ src/Riverside.Labware/ConnectionWindow.xaml.cs
@@ -0,0 +1,392 @@
+﻿using Microsoft.UI;
+using Microsoft.UI.Content;
+using Microsoft.UI.Windowing;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Input;
+using Riverside.Labware.Dialogs;
+using Riverside.Labware.Helpers;
+using Riverside.Labware.Core.PInvoke.Comctl32;
+using Riverside.Labware.Core.PInvoke.User32;
+using Riverside.Labware.Core.PInvoke.Uxtheme;
+using System;
+using System.ComponentModel;
+using System.Runtime.InteropServices;
+using Windows.Foundation;
+using Windows.Graphics;
+using Windows.UI;
+using WinUIEx;
+
+namespace Riverside.Labware
+{
+    public sealed partial class ConnectionWindow : WindowEx, INotifyPropertyChanged
+    {
+        private readonly SUBCLASSPROC mainWindowSubClassProc;
+        private readonly SUBCLASSPROC inputNonClientPointerSourceSubClassProc;
+        private readonly ContentCoordinateConverter contentCoordinateConverter;
+        private readonly OverlappedPresenter overlappedPresenter;
+        private bool _isWindowMaximized;
+        public bool IsWindowMaximized
+        {
+            get => _isWindowMaximized;
+
+            set
+            {
+                if (!Equals(_isWindowMaximized, value))
+                {
+                    _isWindowMaximized = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(IsWindowMaximized)));
+                }
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        public Window m_window;
+        public ConnectionWindow()
+        {
+            InitializeComponent();
+            ExtendsContentIntoTitleBar = true;
+            AppWindow.Resize(new SizeInt32(1200, 700));
+            this.CenterOnScreen();
+            SetTitleBar(AppTitleBar);
+            AppWindow.TitleBar.PreferredHeightOption = TitleBarHeightOption.Tall;
+
+            FolderView.Visibility = Visibility.Collapsed;
+            TabsGrid.Margin = new Thickness(212, 48, 0, 32);
+
+            if (ShowHideLibrary.IsChecked == false)
+            {
+                ShowHideLibrary.IsChecked = true;
+            }
+
+            overlappedPresenter = AppWindow.Presenter as OverlappedPresenter;
+            IsWindowMaximized = overlappedPresenter.State is OverlappedPresenterState.Maximized;
+            contentCoordinateConverter = ContentCoordinateConverter.CreateForWindowId(AppWindow.Id);
+
+            SetClassicMenuTheme((Content as FrameworkElement).ActualTheme);
+
+            mainWindowSubClassProc = new SUBCLASSPROC(MainWindowSubClassProc);
+            _ = Comctl32Library.SetWindowSubclass((nint)AppWindow.Id.Value, Marshal.GetFunctionPointerForDelegate(mainWindowSubClassProc), 0, nint.Zero);
+
+            nint inputNonClientPointerSourceHandle = User32Library.FindWindowEx((nint)AppWindow.Id.Value, nint.Zero, "InputNonClientPointerSource", null);
+
+            if (inputNonClientPointerSourceHandle != nint.Zero)
+            {
+                inputNonClientPointerSourceSubClassProc = new SUBCLASSPROC(InputNonClientPointerSourceSubClassProc);
+                _ = Comctl32Library.SetWindowSubclass((nint)AppWindow.Id.Value, Marshal.GetFunctionPointerForDelegate(inputNonClientPointerSourceSubClassProc), 0, nint.Zero);
+            }
+
+            AppWindow.Changed += OnAppWindowChanged;
+
+            SizeChanged += OnSizeChanged;
+        }
+        private void OnActualThemeChanged(FrameworkElement sender, object args)
+        {
+            SetTitleBarTheme(sender.ActualTheme);
+            SetClassicMenuTheme(sender.ActualTheme);
+        }
+        private void SetTitleBarTheme(ElementTheme theme)
+        {
+            AppWindowTitleBar titleBar = AppWindow.TitleBar;
+
+            titleBar.BackgroundColor = Colors.Transparent;
+            titleBar.ForegroundColor = Colors.Transparent;
+            titleBar.InactiveBackgroundColor = Colors.Transparent;
+            titleBar.InactiveForegroundColor = Colors.Transparent;
+            titleBar.ButtonBackgroundColor = Colors.Transparent;
+            titleBar.ButtonInactiveBackgroundColor = Colors.Transparent;
+
+            if (theme is ElementTheme.Light)
+            {
+                titleBar.ButtonForegroundColor = Colors.Black;
+                titleBar.ButtonHoverBackgroundColor = Color.FromArgb(20, 0, 0, 0);
+                titleBar.ButtonHoverForegroundColor = Colors.Black;
+                titleBar.ButtonPressedBackgroundColor = Color.FromArgb(30, 0, 0, 0);
+                titleBar.ButtonPressedForegroundColor = Colors.Black;
+                titleBar.ButtonInactiveForegroundColor = Colors.Black;
+            }
+            else
+            {
+                titleBar.ButtonForegroundColor = Colors.White;
+                titleBar.ButtonHoverBackgroundColor = Color.FromArgb(20, 255, 255, 255);
+                titleBar.ButtonHoverForegroundColor = Colors.White;
+                titleBar.ButtonPressedBackgroundColor = Color.FromArgb(30, 255, 255, 255);
+                titleBar.ButtonPressedForegroundColor = Colors.White;
+                titleBar.ButtonInactiveForegroundColor = Colors.White;
+            }
+        }
+        private static void SetClassicMenuTheme(ElementTheme theme)
+        {
+            if (theme is ElementTheme.Light)
+            {
+                UxthemeLibrary.SetPreferredAppMode(PreferredAppMode.ForceLight);
+            }
+            else
+            {
+                UxthemeLibrary.SetPreferredAppMode(PreferredAppMode.ForceDark);
+            }
+
+            UxthemeLibrary.FlushMenuThemes();
+        }
+        private const string MaximizeGlyph = "\uE923";
+        private const string RestoreGlyph = "\uE922";
+        private void OnSizeChanged(object sender, WindowSizeChangedEventArgs args)
+        {
+            if (TitlebarMenuFlyout.IsOpen)
+            {
+                TitlebarMenuFlyout.Hide();
+            }
+            if (overlappedPresenter is not null)
+            {
+                IsWindowMaximized = overlappedPresenter.State is OverlappedPresenterState.Maximized;
+            }
+        }
+        private void OnAppWindowChanged(AppWindow sender, AppWindowChangedEventArgs args)
+        {
+            if (args.DidPositionChange)
+            {
+                if (TitlebarMenuFlyout.IsOpen)
+                {
+                    TitlebarMenuFlyout.Hide();
+                }
+
+                if (overlappedPresenter is not null)
+                {
+                    IsWindowMaximized = overlappedPresenter.State is OverlappedPresenterState.Maximized;
+                }
+            }
+        }
+        private void OnRestoreClicked(object sender, RoutedEventArgs args)
+        {
+            overlappedPresenter.Restore();
+        }
+        private void OnMoveClicked(object sender, RoutedEventArgs args)
+        {
+            MenuFlyoutItem menuItem = sender as MenuFlyoutItem;
+            if (menuItem.Tag is not null)
+            {
+                ((MenuFlyout)menuItem.Tag).Hide();
+                _ = User32Library.SendMessage((nint)AppWindow.Id.Value, WindowMessage.WM_SYSCOMMAND, 0xF010, 0);
+            }
+        }
+        private void OnSizeClicked(object sender, RoutedEventArgs args)
+        {
+            MenuFlyoutItem menuItem = sender as MenuFlyoutItem;
+            if (menuItem.Tag is not null)
+            {
+                ((MenuFlyout)menuItem.Tag).Hide();
+                _ = User32Library.SendMessage((nint)AppWindow.Id.Value, WindowMessage.WM_SYSCOMMAND, 0xF000, 0);
+            }
+        }
+        private void OnMinimizeClicked(object sender, RoutedEventArgs args)
+        {
+            overlappedPresenter.Minimize();
+        }
+        private void OnMaximizeClicked(object sender, RoutedEventArgs args)
+        {
+            overlappedPresenter.Maximize();
+        }
+        private void OnCloseClicked(object sender, RoutedEventArgs args)
+        {
+            Application.Current.Exit();
+        }
+        private nint MainWindowSubClassProc(nint hWnd, WindowMessage Msg, UIntPtr wParam, nint lParam, uint uIdSubclass, nint dwRefData)
+        {
+            if (Msg is WindowMessage.WM_SYSCOMMAND)
+            {
+                SYSTEMCOMMAND sysCommand = (SYSTEMCOMMAND)(wParam.ToUInt32() & 0xFFF0);
+
+                if (sysCommand is SYSTEMCOMMAND.SC_MOUSEMENU)
+                {
+                    FlyoutShowOptions options = new()
+                    {
+                        Position = new Point(0, 15),
+                        ShowMode = FlyoutShowMode.Standard
+                    };
+                    TitlebarMenuFlyout.ShowAt(null, options);
+                    return 0;
+                }
+                else if (sysCommand is SYSTEMCOMMAND.SC_KEYMENU)
+                {
+                    FlyoutShowOptions options = new()
+                    {
+                        Position = new Point(0, 45),
+                        ShowMode = FlyoutShowMode.Standard
+                    };
+                    TitlebarMenuFlyout.ShowAt(null, options);
+                    return 0;
+                }
+            }
+
+            return Comctl32Library.DefSubclassProc(hWnd, Msg, wParam, lParam);
+        }
+        private nint InputNonClientPointerSourceSubClassProc(nint hWnd, WindowMessage Msg, UIntPtr wParam, nint lParam, uint uIdSubclass, nint dwRefData)
+        {
+            switch (Msg)
+            {
+                case WindowMessage.WM_NCLBUTTONDOWN:
+                    {
+                        if (TitlebarMenuFlyout.IsOpen)
+                        {
+                            TitlebarMenuFlyout.Hide();
+                        }
+                        break;
+                    }
+                case WindowMessage.WM_NCRBUTTONUP:
+                    {
+                        if (wParam.ToUInt32() is 2 && Content is not null && Content.XamlRoot is not null)
+                        {
+                            PointInt32 screenPoint = new(lParam.ToInt32() & 0xFFFF, lParam.ToInt32() >> 16);
+                            Point localPoint = contentCoordinateConverter.ConvertScreenToLocal(screenPoint);
+
+                            FlyoutShowOptions options = new()
+                            {
+                                ShowMode = FlyoutShowMode.Standard,
+                                Position = InfoHelper.SystemVersion.Build >= 22000 ? new Point(localPoint.X / Content.XamlRoot.RasterizationScale, localPoint.Y / Content.XamlRoot.RasterizationScale) : new Point(localPoint.X, localPoint.Y)
+                            };
+
+                            TitlebarMenuFlyout.ShowAt(null, options);
+                        }
+                        return 0;
+                    }
+            }
+            return Comctl32Library.DefSubclassProc(hWnd, Msg, wParam, lParam);
+        }
+        public static void CreateModalWindow(WindowEx parentWindow, WindowEx childWindow, bool summonWindowAutomatically = true, bool blockInput = false)
+        {
+            nint hWndChildWindow = WinRT.Interop.WindowNative.GetWindowHandle(childWindow);
+            nint hWndParentWindow = WinRT.Interop.WindowNative.GetWindowHandle(parentWindow);
+            _ = SetWindowLong(hWndChildWindow, GWL_HWNDPARENT, hWndParentWindow);
+            (childWindow.AppWindow.Presenter as OverlappedPresenter).IsModal = true;
+            if (blockInput == true)
+            {
+                _ = EnableWindow(hWndParentWindow, false);
+                childWindow.Closed += ChildWindow_Closed;
+                void ChildWindow_Closed(object sender, Microsoft.UI.Xaml.WindowEventArgs args)
+                {
+                    _ = EnableWindow(hWndParentWindow, true);
+                }
+            }
+            if (summonWindowAutomatically == true)
+            {
+                _ = childWindow.Show();
+            }
+        }
+        private static nint SetWindowLong(nint hWnd, int nIndex, nint dwNewLong)
+        {
+            return nint.Size == 4 ? SetWindowLongPtr32(hWnd, nIndex, dwNewLong) : SetWindowLongPtr64(hWnd, nIndex, dwNewLong);
+        }
+        [DllImport("User32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
+        private static extern bool EnableWindow(nint hWnd, bool bEnable);
+
+        private const int GWL_HWNDPARENT = -8;
+
+        [DllImport("User32.dll", CharSet = CharSet.Auto, EntryPoint = "SetWindowLong")]
+        private static extern nint SetWindowLongPtr32(nint hWnd, int nIndex, nint dwNewLong);
+
+        [DllImport("User32.dll", CharSet = CharSet.Auto, EntryPoint = "SetWindowLongPtr")]
+        private static extern nint SetWindowLongPtr64(nint hWnd, int nIndex, nint dwNewLong);
+        private void HideLibrary_Click(object sender, RoutedEventArgs e)
+        {
+            LibraryPanel.Visibility = Visibility.Collapsed;
+            TabsGrid.Margin = new Thickness(0, 48, 0, 32);
+            FolderView.Margin = new Thickness(0, 0, 0, 32);
+
+            if (ShowHideLibrary.IsChecked == true)
+            {
+                ShowHideLibrary.IsChecked = false;
+            }
+        }
+        private void CreateNewVM_Click(object sender, RoutedEventArgs e)
+        {
+            VMCreationDialog logWin = new();
+            CreateModalWindow(App.m_window, logWin, true, true);
+            App.currentWizardWin = logWin;
+        }
+        private void NewWindow_Click(object sender, RoutedEventArgs e)
+        {
+            m_window = new MainWindow();
+            m_window.Activate();
+        }
+        private void Open_Click(object sender, RoutedEventArgs e)
+        {
+            FeatureNotAvailableDialog logWin = new();
+            CreateModalWindow(App.m_window, logWin, true, true);
+        }
+        private void ShowHideLibrary_Click(object sender, RoutedEventArgs e)
+        {
+            if (LibraryPanel.Visibility == Visibility.Visible)
+            {
+                LibraryPanel.Visibility = Visibility.Collapsed;
+                TabsGrid.Margin = new Thickness(0, 48, 0, 32);
+                FolderView.Margin = new Thickness(0, 0, 0, 32);
+                ShowHideLibrary.IsChecked = false;
+            }
+            else
+            {
+                LibraryPanel.Visibility = Visibility.Visible;
+                TabsGrid.Margin = new Thickness(212, 48, 0, 32);
+                FolderView.Margin = new Thickness(202, 0, 0, 32);
+                ShowHideLibrary.IsChecked = true;
+            }
+        }
+        private void HideFolderView_Click(object sender, RoutedEventArgs e)
+        {
+            FolderView.Visibility = Visibility.Collapsed;
+            TabsView.Margin = new Thickness(0, 0, 0, 0);
+            ShowHideFolderView.IsChecked = false;
+        }
+        private void ShowHideFolderView_Click(object sender, RoutedEventArgs e)
+        {
+            if (FolderView.Visibility == Visibility.Visible)
+            {
+                FolderView.Visibility = Visibility.Collapsed;
+                TabsView.Margin = new Thickness(0, 0, 0, 0);
+                ShowHideFolderView.IsChecked = false;
+            }
+            else
+            {
+                FolderView.Visibility = Visibility.Visible;
+                TabsView.Margin = new Thickness(0, 0, 0, 152);
+                ShowHideFolderView.IsChecked = true;
+            }
+        }
+        private void VMSettings_Click(object sender, RoutedEventArgs e)
+        {
+            VMSettingsDialog logWin = new();
+            CreateModalWindow(App.m_window, logWin, true, true);
+        }
+        private void About_Click(object sender, RoutedEventArgs e)
+        {
+            AboutDialog logWin = new();
+            CreateModalWindow(App.m_window, logWin, true, true);
+        }
+        private void Button_RightTapped(object sender, RightTappedRoutedEventArgs e)
+        {
+            FlyoutBase.ShowAttachedFlyout((FrameworkElement)sender);
+        }
+        private void Button_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            FlyoutBase.ShowAttachedFlyout((FrameworkElement)sender);
+        }
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+        private void MessageLog_Click(object sender, RoutedEventArgs e)
+        {
+            MessageLogDialog logWin = new();
+            CreateModalWindow(App.m_window, logWin, true, true);
+        }
+        private void VMMessageLog_Click(object sender, RoutedEventArgs e)
+        {
+            MessageLogDialog logWin = new();
+            CreateModalWindow(App.m_window, logWin, true, true);
+        }
+        private void Preferences_Click(object sender, RoutedEventArgs e)
+        {
+            SettingsDialog logWin = new();
+            CreateModalWindow(App.m_window, logWin, true, true);
+        }
+    }
+}
